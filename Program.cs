6bc896d95eba78593a3aa6f852d3e2a0030e78f5
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-10 21:34:28 +0530

    Arithmetic operators task1

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@
 int a = 10;
 int b = 20;
 Console.WriteLine(a+b);
+Console.WriteLine(b-a);
+float mul = a * b;
+Console.WriteLine(mul);
+float div=a / b;
+Console.WriteLine(div);
+int modulus = a % b;
+Console.WriteLine(modulus);
--- obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c85ecf4a30cbf84d05c43d5611df5292103a7cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc2661f253f1ef7013f35ab3bf6aa41079ecf95")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
