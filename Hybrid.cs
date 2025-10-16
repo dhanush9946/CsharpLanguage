afc0dd5ec8009f184d91dfc2a10f5b35fe204b2d
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-16 15:29:55 +0530

    Hybrid inheritance

---
+++ Hybrid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class Hybrid
+    {
+        interface IA
+        {
+            void showA();
+        }
+        class Base
+        {
+            public void BaseMethod()
+            {
+                Console.WriteLine("base method");
+            }
+        }
+        class Derived1 : Base, IA
+        {
+            public void showA()
+            {
+                Console.WriteLine("Form A");
+            }
+        }
+        class Derived2 : Derived1
+        {
+            public void AnotherMethod()
+            {
+                Console.WriteLine("Another method");
+            }
+        }
+        public static void Main29()
+        {
+            Derived2 d = new Derived2();
+            d.showA();
+            d.BaseMethod();
+            d.AnotherMethod();
+        }
+    }
+}
