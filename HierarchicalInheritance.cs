b0aec34cf6b0dd71209374ce3de70eb206fb72d7
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-16 15:28:05 +0530

    Hierarchical inheritance

---
+++ HierarchicalInheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class HierarchicalInheritance
+    {
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+        class Dog : Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Barking...");
+            }
+        }
+        class Cat:Animal
+        {
+            public void Meow()
+            {
+                Console.WriteLine("Meowing...");
+            }
+        }
+
+        public static void Main26()
+        {
+            Cat c = new Cat();
+            c.Eat();
+            c.Meow();
+
+            Dog d = new Dog();
+            d.Eat();
+            d.Bark();
+        }
+    }
+}
