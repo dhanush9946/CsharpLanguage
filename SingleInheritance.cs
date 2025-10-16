b2eb628bc945ba009e135d017b0eee98f30d4dac
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-16 15:16:58 +0530

    single inheritance

---
+++ SingleInheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class SingleInheritance
+    {
+        class Animals
+        {
+            public  void Eat()
+            {
+                Console.WriteLine("Eating...");
+            }
+        }
+        class Dog : Animals
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Braking...");
+            }
+        }
+        public static void Main24()
+        {
+            Dog d = new Dog();
+            d.Eat();
+            d.Bark();
+        }
+    }
+}
