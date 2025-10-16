6f21cf44f4dc26092d007dab413efb686d03ddf5
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-16 15:28:57 +0530

    Multiple inheritance

---
+++ MultipleInheritance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class MultipleInheritance
+    {
+        interface IA
+        {
+            void showA();
+        }
+        interface IB
+        {
+            void showB();
+        }
+        class C : IA, IB
+        {
+            public void showA()
+            {
+                Console.WriteLine("Form A");
+            }
+            public void showB()
+            {
+                Console.WriteLine("Form B");
+            }
+        }
+        public static void Main27()
+        {
+            C c = new C();
+            c.showA();
+            c.showB();
+        }
+    }
+}
