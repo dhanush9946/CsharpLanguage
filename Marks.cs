87ca49a74c12ea9ccdd91e569f57086ba2ff94ac
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-10-29 23:41:29 +0530

    Linq highest mark

---
+++ Marks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class Marks
+    {
+        public static void Main46()
+        {
+            List<int> marks = new List<int>() { 2, 3, 5, 6, 55, 33, 32, 56, };
+
+            var max = marks.OrderByDescending(i => i).First();
+            Console.WriteLine(max);
+
+        }
+    }
+}
